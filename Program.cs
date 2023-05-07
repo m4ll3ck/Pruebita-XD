b40ec07bf2d1e35271fa69e7e447cbe02ed6d6d6
Jecy2305 <Edu_Cordova1@hotmail.com>
2023-05-06 20:55:43 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebita_XD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
